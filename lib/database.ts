7f6ddc3c6c0d932a34ced78dad33d39a365c8303
mosscarlos <mosspasantias@gmail.com>
2025-07-01 04:35:36 -0500

    actbasededato

--- lib/database.ts
+++ lib/database.ts
@@ -1,14 +1,26 @@
 import mysql from "mysql2/promise"
 
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_NAME ||
+  !process.env.DB_PORT
+) {
+  throw new Error("🚨 Faltan variables de entorno para conectarse a la base de datos.")
+}
+
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "danny",
-  database: process.env.DB_NAME || "foodscrap",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT), // 👈 importante convertirlo a número
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 }
+
 
 let pool: mysql.Pool | null = null
 
